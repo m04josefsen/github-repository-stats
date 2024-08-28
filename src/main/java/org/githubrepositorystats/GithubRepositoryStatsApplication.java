5b4c3423caaeb79bcf3bd5224d45c3afd8f7b969
Marco Josefsen <majos6959@oslomet.no>
2024-08-28 19:33:51 +0200

    initial commit

---
+++ src/main/java/org/githubrepositorystats/GithubRepositoryStatsApplication.java
@@ -0,0 +1,13 @@
+package org.githubrepositorystats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/githubrepositorystats/GithubRepositoryStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.githubrepositorystats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
