81163749a84311a7573ef7a2a5b1e8b7bbb8bf55
Marco Josefsen <majos6959@oslomet.no>
2024-09-14 15:32:37 +0200

    Made HashMap which holds amount of days in each month, created html content dynamicly based on days in month, and colored the active days, created heatmap.css

--- src/main/java/org/githubrepositorystats/ControllerAndService/HeatmapService.java
+++ src/main/java/org/githubrepositorystats/ControllerAndService/HeatmapService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Year;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -18,28 +19,31 @@ public class HeatmapService {
     public static String generateHtmlForHeatmap(List<Commit> commits, int year) {
         activityCount = new int[12][31];
 
-        /*
-        // If leap year
-        if(year % 4 == 0) {
-            activityCount = new int[366];
-        }
-        else {
-            activityCount = new int[365];
-        }
-         */
-
-        fillActivityCount(commits);
+         fillActivityCount(commits);
 
         StringBuilder html = new StringBuilder();
         html.append("<!DOCTYPE html>");
         html.append("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
         html.append("<head><meta charset='UTF-8'/>");
-        html.append(cssHeatmap());
+        html.append("<link rel='stylesheet' href='src/main/resources/static/heatmap.css'/>");
         html.append("</head><body><div class='container'><h1>Commit heatmap</h1>");
 
-        appendHeatmapHtml(html,commits);
+        appendHeatmapHtml(html, year);
 
-        html.append("</div></body></html>");
+        html.append("</div></body>");
+
+        /*
+        html.append("<script>");
+        // JavaScript to load CSS dynamically
+        html.append("var link = document.createElement('link');");
+        html.append("link.rel = 'stylesheet';");
+        html.append("link.type = 'text/css';");
+        html.append("link.href = 'src/main/resources/static/heatmap.css';");
+        html.append("document.head.appendChild(link);");
+        html.append("</script>");
+         */
+
+        html.append("</html>");
         return html.toString();
     }
 
@@ -49,38 +53,79 @@ public class HeatmapService {
         return generateHtmlForHeatmap(commits, currentYear);
     }
 
-    // Helper method to append heatmap HTML details
-    private static void appendHeatmapHtml(StringBuilder html, List<Commit> commits) {
-        // TODO: denne må bruke activity count, if a.length == 366... else...
+    // Helper method to append heatmap HTML details for normal years
+    private static void appendHeatmapHtml(StringBuilder html, int year) {
+        HashMap<Integer, Integer> map = fillMonthsWithDays();
+
+        // If leap year, change feburary from 28 -> 29;
+        if(year % 4 == 0) {
+            map.replace(2, 29);
+        }
+
+        html.append("<div class='heatmap' >");
+
+        // Loop through each month to create a new div, then loop through each month to add the days
+        for(int i = 0; i < 12; i++) {
+            int days = map.get(i+1);
+
+            html.append("<div class='month'>");
+
+            for(int j = 0; j < days; j++) {
+                int commitCount = activityCount[i][j];
+
+                // TODO: midlertidig, kun for test, skal endres for mørkere farge hvis flere commits
+                if(commitCount > 0) {
+                    html.append("<div class='day active'>"); // Used as a box for coloring
+                }
+                else {
+                    html.append("<div class='day inactive'>"); // Used as a box for coloring
+                }
+
+                html.append("</div>"); // Closing day
+            }
+
+            html.append("</div>"); // Closing month
+        }
+
+        html.append("</div>"); // Closing heatmap
     }
 
-    // Styling for Heatmap
-    public static String cssHeatmap() {
-        String css = "<style>";
-
-        css += "</style>";
-
-        return css;
-    }
-
+    // Method to fill activity count array with dates
     private static void fillActivityCount(List<Commit> commits) {
-        // TODO: loop gjennom commits, der dato matcher fyll verdi++;
 
         for(Commit c : commits) {
             LocalDate d = c.getDate();
             int day = d.getDayOfMonth();
             int month = d.getMonthValue();
-            System.out.println(d);
-            System.out.println("Day: " + day + " - Month: " + month);
-            System.out.println("---");
 
             activityCount[month-1][day-1]++;
         }
 
+        /*
         for(int i = 0; i < 12; i++) {
             for(int j = 0; j < 31; j++) {
                 System.out.println(activityCount[i][j]);
             }
         }
+         */
+    }
+
+    // Method to fill how many days each month has
+    private static HashMap<Integer, Integer> fillMonthsWithDays() {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 31);
+        map.put(2, 28);
+        map.put(3, 31);
+        map.put(4, 30);
+        map.put(5, 31);
+        map.put(6, 30);
+        map.put(7, 31);
+        map.put(8, 31);
+        map.put(9, 30);
+        map.put(10, 31);
+        map.put(11, 30);
+        map.put(12, 31);
+
+        return map;
     }
 }
